1618f3db944f2156224d2a9f3b727b577805eac7
angiepacaud <angie.pacaud@gmail.com>
2018-03-04 21:20:25 +0100

    view

--- src/Blog/ArticlesBundle/Controller/DefaultController.php
+++ src/Blog/ArticlesBundle/Controller/DefaultController.php
@@ -51,8 +51,17 @@ class DefaultController extends Controller
 
     public function viewAction($id)
   {
-	  return $this->render('BlogArticlesBundle:Default:view.html.twig', array(
-      'id' => $id
+	$advert = array(
+ 	'title'   => 'Article 1',
+    'id'      => $id,
+    'author'  => 'User 1',
+    'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
+    );
+
+    return $this->render('BlogArticlesBundle:Default:view.html.twig', array(
+      'advert' => $advert
+
     ));
   
   }
