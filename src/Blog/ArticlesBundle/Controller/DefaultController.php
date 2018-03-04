048d68108642144afec6d8cef6b06a76aaeaa738
angiepacaud <angie.pacaud@gmail.com>
2018-03-04 20:48:21 +0100

    page home

--- src/Blog/ArticlesBundle/Controller/DefaultController.php
+++ src/Blog/ArticlesBundle/Controller/DefaultController.php
@@ -10,13 +10,44 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-         $content = $this->get('templating')->render('BlogArticlesBundle:Default:index.html.twig', array('listAdverts' => array()
-         	));
-    
-    return new Response($content);
+         $listAdverts = array(
+      array(
+        'title'   => 'Article 1',
+        'id'      => 1,
+        'author'  => 'User 1',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        ),
+        
+      array(
+        'title'   => 'Article 2',
+        'id'      => 2,
+        'author'  => 'User 2',
+        'content' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+        )
+        
+    );
+
+    // Et modifiez le 2nd argument pour injecter notre liste
+    return $this->render('BlogArticlesBundle:Default:index.html.twig', array(
+      'listAdverts' => $listAdverts
+    ));
 
     }
 
+  public function menuAction($limit)
+  {
+    // On fixe en dur une liste ici, bien entendu par la suite
+    // on la récupérera depuis la BDD !
+    $listAdverts = array(
+      array('id' => 1, 'title' => 'Article 1'),
+      array('id' => 2, 'title' => 'Article 2'),
+      array('id' => 3, 'title' => 'Article 3')
+    );
+
+    return $this->render('BlogArticlesBundle:Default:menu.html.twig', array(
+      'listAdverts' => $listAdverts
+    ));
+  }
 
     public function viewAction($id)
   {
