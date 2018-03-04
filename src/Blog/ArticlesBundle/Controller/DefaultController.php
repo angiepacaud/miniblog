898fb6078dfd9dd2b281d062d23c90880206fd56
angiepacaud <angie.pacaud@gmail.com>
2018-03-04 18:38:50 +0100

    controller

--- src/Blog/ArticlesBundle/Controller/DefaultController.php
+++ src/Blog/ArticlesBundle/Controller/DefaultController.php
@@ -13,18 +13,60 @@ class DefaultController extends Controller
          $content = $this->get('templating')->render('BlogArticlesBundle:Default:index.html.twig', array('nom' => 'articles'));
     
     return new Response($content);
+
     }
 
-    public function viewAction($id, Request $request)
 
+    public function viewAction($id)
   {
-	  $tag = $request->query->get('contenu');
-
-    return $this->render('BlogArticlesBundle:Default:view.html.twig', array(
-      'id'  => $id,
-      'contenu' => $tag,
+	  return $this->render('BlogArticlesBundle:Default:view.html.twig', array(
+      'id' => $id
     ));
+  
   }
+
+
+   public function addAction(Request $request)
+  {
+
+    if ($request->isMethod('POST')) {
+
+      $request->getSession()->getFlashBag()->add('article', 'Article bien enregistré.');
+
+      return $this->redirectToRoute('blog_articles_view', array('id' => 1));
+
+    }
+
+    return $this->render('BlogArticlesBundle:Default:add.html.twig');
+
+  }
+
+
+  public function editAction($id, Request $request)
+  {
+
+    if ($request->isMethod('POST')) {
+
+      $request->getSession()->getFlashBag()->add('notice', 'Article bien modifié.');
+
+      return $this->redirectToRoute('blog_articles_view', array('id' => 5));
+
+    }
+
+    return $this->render('BlogArticlesBundle:Default:edit.html.twig');
+
+  }
+
+
+
+  public function deleteAction($id)
+  {
+
+    return $this->render('BlogArticlesBundle:Default:delete.html.twig');
+
+  }
+
+
 
 
 }
